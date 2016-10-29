9e5e9b66466910ceff700ef30f989edd28fb0daa
Dusan <dreamer.h@gmail.com>
2016-10-29 20:42:38 +0200

    Expand hello world message with slightly less love this time

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package quickstart;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! I love you so much!");
+        System.out.println("Hello, World! I love you so much... though, not really that much.");
     }
 }
